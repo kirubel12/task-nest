372074e8dc27455bd836bca7fb493bdf4fe643b3
kirubel12 <kirubelhabte1991@gmail.com>
2025-10-12 09:59:44 +0300

    feat:setup shadcn for UI/components

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
+import {Button} from "@/components/ui/button";
 
 
 export default function Home() {
   return (
    <div>
        <h1>Task Nest</h1>
+       <Button>Get started</Button>
    </div>
   );
 }
